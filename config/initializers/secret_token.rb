16eaa578900eafc2de64f0b34642cc4f95ad4d92
Chris Kimpton <chris@kimptoc.net>
2011-07-26 17:51:47 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UnicodeParallelBug::Application.config.secret_token = '56894f384ffa556ee005a0d6e371fe4fa44b61d4dcbfd033b86cc94accf784bca169c7e485666ff667e1538984e91137c446793d83c713103c4d64e6944d608b'
